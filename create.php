84d60baa3f21e408b5b7cfdd4de69ba96f59fd80
JoosepJõeleht <jissepo@tlu.ee>
2015-11-16 19:00:44 +0200

    Starting again
    Otsustasime siis jaaniga, et me alustame algusest koodi kirjutamisega.
    Klassid ei tahtnud funktsioneerida ning kõik oli kuidagi katki.

--- create.php
+++ create.php
@@ -1,100 +1,42 @@
-<?php 
+<?php
 	require_once("functions.php");
-
-	//paneme ühenduse kinni
-	function cleanInput($data){
-  	$data = trim($data);
-  	$data = stripslashes($data);
-  	$data = htmlspecialchars($data);
-  	return $data;
+	
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if (empty($_POST["username"])) {
+		$username_error = "Name is required";
+		} else {
+		$username = test_input($_POST["name"]);
+		}
 	}
-	
-$email_error = "";
-$pw_error = "";
-$gender_error = "";
-$email = "";
-	if (isset($_POST["create"]) ){
-						
-	//kontrollime eposti et poleks tühi
-	
-				if(empty($_POST["email"])){
-					$email_error = "Insert e-mail";
-				} else {
-					$email = cleanInput($_POST["email"]);
-				}
-				if(empty($_POST["password"])){
-					$pw_error = "Insert password";
-				} else {
-					//kontrollime pikkust
-				if(strlen($_POST["password"]) < 8){
-					$pw_error = "Has to be atleast 8 characters";
-					}else{
-					$password = cleanInput($_POST["password"]);
-				}
-			}
-			if ($_POST["gender"]){
-					$gender = cleanInput($_POST["gender"]);
-				}
-			if ($_POST["First_name"]){
-					
-					$First_name = cleanInput($_POST["First_name"]);
-				}
-			if ($_POST["Last_name"]){
-					$Last_name = cleanInput($_POST["Last_name"]);
-				}
-			if ($_POST["Address"]){
-					$Address = cleanInput($_POST["Address"]);
-				}
-			
-			if(	$email_error == "" && $pw_error == ""){
-				$hash = hash("sha512", $password);
-				createUser($email, $hash, $First_name, $Last_name, $Address);
-				
-        }
-	  }
-	?>
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if (empty($_POST["password"])) {
+		$pw_error = "Password is required";
+		} else {
+		$password = test_input($_POST["password"]);
+		}
+	}
+?>
+
 <?php 
 	$page_title = "Register";
 	$page_file_name = "create.php";
 	if(isset($_SESSION['logged_in_user_id'])){
-		header("Location: data.php");
+		header("Location: index.php");
 		
 	}
 ?>
-	<?php require_once("header.php");?>
-	
-<link async href="http://fonts.googleapis.com/css?family=Antic" data-generated="http://enjoycss.com" rel="stylesheet" type="text/css"/>
-<link rel="stylesheet" href="button.css">
-		<div class="center">
+<?php require_once("header.php"); ?>
 
-			<div class="text">Create User</div>
-			<form action="create.php" method="post">
-			
-				<p>Email/Username</p>
-				<input class="button" name="email" type="email" placeholder="@example.com" value="<?php echo $email;?>" >* <?php echo $email_error;?> 
-				
-				<p>Password</p>
-				<input class="button" name="password" type="password" placeholder="Parool" >* <?php echo $pw_error;?>
-				<br>
-				<p>Gender</p>
-				<select name="gender" style="width:173px;"> 
-					<option value="0">Male</option> 
-					<option value="1">Female</option>  
-				</select> <?php echo $gender_error;?> 
-				<br>
-				<p>First name</p>
-				<input class="button" name="First_name" type="text" placeholder="example: John" >
-				<br>
-				<p>Last name</p>
-				<input class="button" name="Last_name" type="text" placeholder="example: Doe" >
-				<br>
-				<p>Address</p>
-				<input class="button" name="Address" type="text" placeholder="example: Harjumaa" >
-				<br><br>
-				
-				<input name="create" type="submit" value="Create User">
-				
-			</form>	
-			</div>
-			
+<div class="text">Create User</div>
+	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+	
+		<p>Email/Username</p>
+		<input class="button" name="username" type="text" placeholder="example" value="<?php echo $username;?>" >* <?php echo $username_error;?> 
+		
+		<p>Password</p>
+		<input class="button" name="password" type="password" placeholder="Password" >* <?php echo $pw_error;?>
+		<br><br>
+		<input name="create" type="submit" value="Create User">
+		<br><br>
+	</form>	
 <?php require_once("footer.php"); ?>
--- functions.php
+++ functions.php
@@ -1,24 +1,8 @@
-function cleanInput($data) {
+<?php 
+function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
   return $data;
 }
-require_once("../../config_global.php");
-session_start();
-
-
-/*// Create connection
-$mysqli = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);*/
-//}
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$User = new User($mysqli);
+?>
